3c0a059bd29e4d6c6c4f815545b250146d7ac6bb
dong <dong.22@kakaoenterprise.com>
2023-08-19 16:15:47 +0900

    good

--- src/main/kotlin/goormton/Solve1W.kt
+++ src/main/kotlin/goormton/Solve1W.kt
@@ -1,8 +1,59 @@
 package goormton
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
 fun main(args: Array<String>) {
-    main0814(args)
+    //main0814(args)
+    main0818(args)
 }
+
+fun main0818(args: Array<String>) {
+
+    val (count, indexOfAnswer) = readLine()!!.split(" ").map { s -> s.toInt() }
+    val scanner = Scanner(System.`in`)
+
+    val unOrderedList: List<Int> = (1..count)
+        .asSequence()
+        .map { scanner.next().toInt() }
+        .toList()
+
+    val orderedList = unOrderedList
+        .sortedWith(
+            compareByDescending<Int> { countOf1AtBinary(it) }
+                .thenByDescending { it }
+        )
+        .toList()
+    print(orderedList[indexOfAnswer - 1])
+
+
+    //helper(unOrderedList,orderedList)
+
+}
+
+fun helper( unOrderedList:List<Int>, orderedList:List<Int>) {
+    println("\n============\n")
+    println(unOrderedList)
+    val elemAndCount =
+        unOrderedList.associate { i -> Pair(i, countOf1AtBinary(i)) }
+    val elemAndBinaryString =
+        unOrderedList.associate { i -> Pair(i, Integer.toBinaryString(i)!!) }
+    println(elemAndCount)
+    println(elemAndBinaryString)
+
+    println(orderedList)
+
+}
+
+fun countOf1AtBinary(decimalNumber: Int): Int {
+    return Integer.toBinaryString(decimalNumber)
+        .toCharArray()
+        .filter { it -> it == '1' }
+        .size
+}
+
+// ----
 
 fun main0814(args: Array<String>) {
     val (w, r) = readLine()!!
@@ -69,5 +120,47 @@ fun calculateExpression(expression: String): Int {
 }
 
 fun operate(i1: Int, i2: Int, op: (Int, Int) -> Int): Int {
-    return op(i1,i2)
+    return op(i1, i2)
 }
+
+
+fun main0817(args: Array<String>) {
+    readLine()
+    val comps: List<Int> = readLine()!!
+        .split(" ")
+        .map { s -> s.toInt() }.toList()
+    val max: Int = comps.maxOf { it.toInt() }
+    when (isIncreasing(0, comps.indexOf(max), comps) &&
+            isDecreasing(comps.indexOf(max), comps.size, comps)
+    ) {
+        true -> print(comps.sum())
+        else -> {
+            print("0")
+        }
+    }
+}
+
+fun isDecreasing(beginIndex: Int, endIndex: Int, collection: List<Int>): Boolean {
+    var pointValue: Int = collection[beginIndex]
+    for (i in beginIndex until endIndex) {
+        if (pointValue >= collection[i]) {
+            pointValue = collection[i]
+        } else {
+            return false
+        }
+    }
+    return true
+}
+
+fun isIncreasing(beginIndex: Int, endIndex: Int, collection: List<Int>): Boolean {
+    var pointValue: Int = collection[beginIndex]
+    for (i in beginIndex until endIndex) {
+        if (pointValue <= collection[i]) {
+            pointValue = collection[i]
+        } else {
+            return false
+        }
+    }
+    return true
+}
+
--- src/main/kotlin/leetcode/June.kt
+++ src/main/kotlin/leetcode/June.kt
@@ -1,12 +1,62 @@
 package leetcode
 
 import java.util.*
+import kotlin.math.abs
 
 fun main() {
-    print("4가 나와야 함 : ")
-    println(balancedStringSplit("RLRRLLRLRL"))
-
+//    print("4가 나와야 함 : ")
+//    println(balancedStringSplit("RLRRLLRLRL")
+    //print("9 나와야 한다 : ")
+    //println(differenceOfSum(intArrayOf(1, 15, 6, 3)))
+    println(cellsInRange("K1:L2"))
 }
+
+
+fun cellsInRange(s: String): List<String> {
+    val answerList: MutableList<String> = mutableListOf()
+    val (begin, end) = s.split(":")
+    for (alphabetNumber in begin.toByteArray()[0].toInt()..end.toByteArray()[0].toInt()) {
+        val alphabet:Char = alphabetNumber.toChar()
+        for (rowNumber in begin.substring(1, 2).toInt()..end.substring(1, 2).toInt()) {
+            answerList.add(alphabet.toString() + rowNumber.toString())
+        }
+    }
+    return answerList
+}
+
+
+//
+
+
+fun differenceOfSum(nums: IntArray): Int {
+    val i1 = sumOfElements(nums)
+    val i2 = sumOfDigits(nums)
+    return abs(i1 - i2).toInt()
+}
+
+fun sumOfDigits(nums: IntArray): Int {
+    val compList: MutableList<Int> = mutableListOf();
+    for (i in nums.toList()) {
+        decompositionAndAddToList(compList, i)
+    }
+    return compList.sum()
+}
+
+fun decompositionAndAddToList(compList: MutableList<Int>, i: Int) {
+    when {
+        i < 10 -> compList.add(i)
+        else -> {
+            compList.add(i % 10)
+            decompositionAndAddToList(compList, i / 10)
+        }
+    }
+}
+
+fun sumOfElements(nums: IntArray): Int {
+    return nums.sum()
+}
+
+//
 
 fun balancedStringSplit(s: String): Int {
     val stack = Stack<Int>()
@@ -22,8 +72,8 @@ fun balancedStringSplit(s: String): Int {
     return answer;
 }
 
-fun isBalanced(stack: Stack<Int>) : Boolean {
-   return  stack.toList().sum() == 0
+fun isBalanced(stack: Stack<Int>): Boolean {
+    return stack.toList().sum() == 0
 }
 
 fun converterRL(s: Char): Int {
@@ -35,27 +85,35 @@ fun converterRL(s: Char): Int {
 }
 
 
-
 fun numberOfSteps(num: Int): Int {
     return when {
         num == 0 -> return 0
-        num%2 == 0 -> stepEven(num/2,1)
-        else -> stepOdd(num-1,1)
+        num % 2 == 0 -> stepEven(num / 2, 1)
+        else -> stepOdd(num - 1, 1)
     }
 }
 
-fun stepOdd(num: Int, depth: Int):Int {
+fun stepOdd(num: Int, depth: Int): Int {
     return when {
-        num ==0 -> depth;
-        num%2 == 0 -> stepEven(num/2,depth+1)
-        else -> stepOdd(num-1,depth+1)
+        num == 0 -> depth;
+        num % 2 == 0 -> stepEven(num / 2, depth + 1)
+        else -> stepOdd(num - 1, depth + 1)
     }
 }
 
-fun stepEven(num: Int, depth: Int) :Int{
+fun stepEven(num: Int, depth: Int): Int {
     return when {
-        num ==0 -> depth
-        num%2 == 0 -> stepEven(num/2,depth+1)
-        else -> stepOdd(num-1,depth+1)
+        num == 0 -> depth
+        num % 2 == 0 -> stepEven(num / 2, depth + 1)
+        else -> stepOdd(num - 1, depth + 1)
+    }
+}
+
+
+class OrderedStream(n: Int) {
+    val list = Array<String>(n + 1) { "" } // 초기 크기와 기본값 지정
+    fun insert(idKey: Int, value: String): List<String> {
+        list[idKey] = value;
+        return list.toList()
     }
 }

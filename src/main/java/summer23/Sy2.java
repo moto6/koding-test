583580c996fd7fd8e4c5385bc143b11f963332fd
dong <dong.22@kakaoenterprise.com>
2023-08-29 16:59:13 +0900

    g

--- src/main/java/summer23/Sy2.java
+++ src/main/java/summer23/Sy2.java
@@ -4,46 +4,95 @@ import java.util.List;
 
 public class Sy2 {
 
+    Integer direction = 0;
+    List<MyPair> diffs = List.of(
+        new MyPair(0, 1),
+        new MyPair(1, 0),
+        new MyPair(0, -1),
+        new MyPair(-1, 0)
+    );
+    final int LOOP_THRESHOLD = 10;
+
     public static void main(String[] args) {
         Sy2 sy2 = new Sy2();
-        sy2.convertToMap(new String[]{"...X..", "....XX", "..X..."});
+        System.out.println(sy2.solution(new String[]{"...X..", "....XX", "..X..."}));
+        System.out.println(sy2.solution(new String[]{"....X..", "X......", ".....X.", "......."}));
+        //15 나와야함
     }
 
     public int solution(String[] R) {
-        int[][] vistied = new int[R[0].length()][R.length];
-        final int LOOP_THREASHOLD = 7;
-        List<Diff> diffs = List.of(
-            new Diff(0, 1),
-            new Diff(1, 0),
-            new Diff(0, -1),
-            new Diff(-1, 0)
-        );
+        int[][] visited = new int[R.length][R[0].length()];
         char[][] map = convertToMap(R);
+        MyPair currentPos = new MyPair(0, 0);
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            visited[currentPos.first][currentPos.second]++;
+            if (visited[currentPos.first][currentPos.second] > LOOP_THRESHOLD + 1) {
+                break;
+            }
+            currentPos = availableNextPos(currentPos, map);
+        }
+        return cleanedAreaSize(visited);
+    }
 
-//        for (int i = 0; i < Integer.MAX_VALUE; i++) {
-//
-//        }
-        return -1;
+    private int cleanedAreaSize(int[][] visited) {
+        int count = 0;
+        for (int[] line : visited) {
+            for (int elem : line) {
+                if (elem != 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 
+    private MyPair availableNextPos(
+        MyPair currentPos,
+        char[][] map) {
+
+        int nextFirst = diffs.get(direction % 4).first + currentPos.first;
+        int nextSecond = diffs.get(direction % 4).second + currentPos.second;
+        if (rangeAvabilable(map.length, map[0].length, nextFirst, nextSecond) && mapAvabilable(nextFirst,nextSecond,map)) {
+            return new MyPair(nextFirst, nextSecond);
+        }
+        direction++;
+        return currentPos;
+    }
+
+    private boolean mapAvabilable(int nextFirst, int nextSecond, char[][] map) {
+        return map[nextFirst][nextSecond] == '.';
+    }
+
+    private static boolean rangeAvabilable(int limitFirst, int limitSecond, int nextFirst,
+        int nextSecond) {
+        return ((limitFirst > nextFirst) &&
+            (limitSecond > nextSecond) &&
+            (0 <= nextFirst) &&
+            (0 <= nextSecond)
+        );
     }
 
     private char[][] convertToMap(String[] r) {
-        char[][] map = new char[r[0].length()][r.length];
+        char[][] map = new char[r.length][r[0].length()];
         for (int i = 0; i < r.length; i++) {
             char[] line = r[i].toCharArray();
-            System.arraycopy(line, 0, map[i], 0, line.length);
+            //System.arraycopy(line, 0, map[i], 0, line.length);
+            //Arrays.cop
+            for (int j = 0; j < line.length; j++) {
+                map[i][j] = line[j];
+            }
         }
         return map;
     }
 
-    private class Diff {
+    private class MyPair {
 
         final int first;
-        final int secode;
+        final int second;
 
-        public Diff(int first, int secode) {
+        public MyPair(int first, int second) {
             this.first = first;
-            this.secode = secode;
+            this.second = second;
         }
     }
 }

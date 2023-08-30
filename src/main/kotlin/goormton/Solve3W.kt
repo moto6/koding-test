3a65f796da87c5bb29c690605a20af9e026c14f3
dong <dong.22@kakaoenterprise.com>
2023-08-30 12:28:16 +0900

    g

--- src/main/kotlin/goormton/Solve3W.kt
+++ src/main/kotlin/goormton/Solve3W.kt
@@ -2,47 +2,90 @@ package goormton
 
 import java.util.*
 
+val diffs: List<Pair<Int, Int>> = listOf(
+    Pair(1, 0),
+    Pair(-1, 0),
+    Pair(0, 1),
+    Pair(0, -1)
+)
+var count = 0
 
 //0829
 fun main(args: Array<String>) {
     val N = readLine()!!.toInt()
-    val visited: MutableList<MutableList<Boolean>> = MutableList(N) { MutableList(N) { false } }
-    //val visited = Array(N) { Array(N) { false } }
-    val diffs: List<Pair<Int, Int>> = listOf(
-        Pair(0, 1),
-        Pair(1, 0),
-        Pair(-1, 0),
-        Pair(0, -1),
-    )
-    val queue: Queue<Pair<Int, Int>> = LinkedList()// 자바의 그것을 사용해야한다
     val matrix: MutableList<MutableList<Int>> = readMatrix(N)
-    var count = 0
-
+    //val visited: MutableList<MutableList<Boolean>> = configMatrix(N)
+    val visited: MutableList<MutableList<Boolean>> = MutableList(N) { MutableList(N) { false } }
 
     for (fi in 0 until N) {
         for (si in 0 until N) {
-            if (isHouse(matrix, fi, si) && isNeverVisited(visited, fi, si)) {
-                queue.add(Pair(fi, si))
-                visited[fi][si] = true
-                while (queue.isNotEmpty()) {
-                    val curCodi: Pair<Int, Int> = queue.poll()
-                    for (step in diffs) {
-                        val nextCodi = Pair(
-                            curCodi.first + step.first,
-                            curCodi.second + step.second
-                        )
-                        if (isInRange(nextCodi, N)) {
-                            queue.add(nextCodi)
-                            visited[nextCodi.first][nextCodi.second] = true
-                            println(visited)
-                        }
-                    }
-                }
-            }
-            count++ //install gen
+            doBfs(fi, si, N, matrix, visited);
         }
     }
     println(count)
+    //println("count = $count")
+}
+
+fun doBfs(
+    fi: Int,
+    si: Int,
+    N: Int,
+    matrix: MutableList<MutableList<Int>>,
+    visited: MutableList<MutableList<Boolean>>,
+) {
+
+    if (isHouse(matrix, fi, si) && isNotVisited(visited, fi, si)) {
+
+        val queue: Queue<Pair<Int, Int>> = LinkedList()// 자바의 그것을 사용해야한다
+        queue.add(Pair(fi, si))
+        visited[fi][si] = true
+
+        while (queue.isNotEmpty()) {
+            //printVisited(visited)
+            val curCodi = queue.poll()
+            //println(queue.size)
+            //println(curCodi)
+
+            for (step in diffs) {
+                val nextCodi = Pair(
+                    curCodi.first + step.first,
+                    curCodi.second + step.second
+                )
+                if (isInRange(nextCodi, N) &&
+                    isHouse(matrix, nextCodi.first, nextCodi.second) &&
+                    isNotVisited(visited, nextCodi.first, nextCodi.second)
+                ) {
+                    //println("GO NEXT = $nextCodi")
+                    queue.add(nextCodi)
+                    visited[nextCodi.first][nextCodi.second] = true
+                }
+            }
+        }
+        count += 1
+    }
+}
+
+fun printVisited(visited: MutableList<MutableList<Boolean>>) {
+    visited.forEach { it ->
+        it.forEach {
+            if (it) {
+                print("■  ")
+            } else {
+                print("□  ")
+            }
+        }
+        println()
+    }
+}
+
+fun configMatrix(N: Int): MutableList<MutableList<Boolean>> {
+    val s: MutableList<MutableList<Boolean>> = mutableListOf()
+    //return MutableList(N) { MutableList(N) { false } }
+    for (i in 0 until N) {
+        val ss: MutableList<Boolean> = MutableList(N) { false }
+        s.add(ss)
+    }
+    return s
 }
 
 fun isInRange(nextCodi: Pair<Int, Int>, size: Int): Boolean {
@@ -50,7 +93,7 @@ fun isInRange(nextCodi: Pair<Int, Int>, size: Int): Boolean {
             (nextCodi.second in 0 until size)
 }
 
-fun isNeverVisited(
+fun isNotVisited(
     visited: MutableList<MutableList<Boolean>>,
     firstIndex: Int,
     secondIndex: Int
